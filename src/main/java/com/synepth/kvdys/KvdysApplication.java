e10d59a0bcd9563dcf85c98e56ae5d640bb2437d
Synepth <ardakoksal5234@gmail.com>
2026-08-03 13:24:28 +0300

    feat: initial spring boot setup with health endpoint

---
+++ src/main/java/com/synepth/kvdys/KvdysApplication.java
@@ -0,0 +1,13 @@
+package com.synepth.kvdys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvdysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KvdysApplication.class, args);
+	}
+
+}
